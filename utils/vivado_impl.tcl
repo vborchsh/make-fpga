d791abcc0e85ecbcfee8d93d5cd57a1cc4780746
Vladislav Borshch <borchsh.vn@gmail.com>
2023-05-11 10:32:58 +0700

    fixed return code of tcl scripts for CI integration

--- utils/vivado_impl.tcl
+++ utils/vivado_impl.tcl
@@ -1,15 +1,22 @@
 set _xil_proj_name_ [lindex $::argv 0]
 set _xil_proj_path_ [lindex $::argv 1]
 
+set run_name impl_1
+
 # Open project
 open_project $_xil_proj_path_/$_xil_proj_name_
 update_compile_order -fileset sources_1
 
 # Implementation
-reset_run impl_1
-launch_runs impl_1 -to_step write_bitstream -jobs 16
-wait_on_run impl_1
+reset_run $run_name
+launch_runs $run_name -to_step write_bitstream -jobs 16
+wait_on_run $run_name
 
 # Done
+set status [get_property STATUS [get_runs $run_name]]
 close_project
-exit
+
+if {$status != "write_bitstream Complete!"} {
+  exit 1
+}
+exit 0
--- utils/vivado_synth.tcl
+++ utils/vivado_synth.tcl
@@ -1,15 +1,22 @@
 set _xil_proj_name_ [lindex $::argv 0]
 set _xil_proj_path_ [lindex $::argv 1]
 
+set run_name synth_1
+
 # Open project
 open_project $_xil_proj_path_/$_xil_proj_name_
 update_compile_order -fileset sources_1
 
 # Synthesis
-reset_run synth_1
-launch_runs synth_1 -jobs 16
-wait_on_run synth_1
+reset_run $run_name
+launch_runs $run_name -jobs 16
+wait_on_run $run_name
 
 # Done
+set status [get_property STATUS [get_runs $run_name]]
 close_project
-exit
+
+if {$status != "synth_design Complete!"} {
+  exit 1
+}
+exit 0
