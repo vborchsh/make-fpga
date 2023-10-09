df04d4590201ce577f3ee90e904e026f8458a476
Vladislav Borshch <borchsh.vn@gmail.com>
2023-10-09 15:50:05 +0700

    v0.5 fix possible BD corrupting. Upgrade standalone IP cores only

--- utils/vivado_ip_upgrade.tcl
+++ utils/vivado_ip_upgrade.tcl
@@ -1,4 +1,5 @@
 # Based on https://github.com/hukenovs/tcl_for_fpga by Kapitanov
+# Upgrade standalone IPs only, not in block design.
 
 set _xil_proj_name_ [lindex $::argv 0]
 set _xil_proj_path_ [lindex $::argv 1]
@@ -6,19 +7,21 @@ set _xil_proj_path_ [lindex $::argv 1]
 # Open project
 open_project $_xil_proj_path_/$_xil_proj_name_
 
-# Report status
-report_ip_status -name ip_status
-
-# Report all cores
+# Collect all cores list
 set ip_cores_list [get_ips]
 
-# Check every core and update locked AND not upgraded
+# Check every core and update `locked` AND not `upgraded` AND not in `BD`
 for {set i 0} {$i < [llength $ip_cores_list]} {incr i} {
     set ip_core_inst [lindex $ip_cores_list $i]
-    
+
     set locked [get_property IS_LOCKED $ip_core_inst]
     set upgrade [get_property UPGRADE_VERSIONS $ip_core_inst]
-    if {$upgrade != "" && $locked} {
+    set inside_bd [get_property IS_BD_CONTEXT $ip_core_inst]
+    if {$upgrade != "" && $locked && !$inside_bd} {
         upgrade_ip $ip_core_inst -log ip_upgrade.log
     }
 }
+
+close_project
+
+exit 0
