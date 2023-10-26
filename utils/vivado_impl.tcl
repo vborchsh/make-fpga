5555eb53b2cf0b84365ee3c39c298d2e6db18e1a
Vladislav Borshch <borchsh.vn@gmail.com>
2023-10-26 12:46:03 +0700

    add timestamp with date and time to `bitstream` file

--- utils/vivado_impl.tcl
+++ utils/vivado_impl.tcl
@@ -15,9 +15,16 @@ wait_on_run $run_name
 
 # Done
 set status [get_property STATUS [get_runs $run_name]]
+set top_module [get_property top [current_fileset]]
 close_project
 
 if {$status != "write_bitstream Complete!"} {
   exit 1
+} else {
+  # Copy bitstream to poject's root folder
+  set curr_time [clock format [clock seconds] -format %d-%m-%Y_%H_%M_%S_]
+  set bit_path $_xil_proj_path_/$_xil_proj_name_.runs/$run_name
+
+  file rename -force $bit_path/$top_module.bit $bit_path/$curr_time$top_module.bit
+  exit 0
 }
-exit 0
